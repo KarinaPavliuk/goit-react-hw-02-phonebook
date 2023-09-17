46bca72e53f5ce812dc3ffde7ee5a5ec8523d868
KarinaPavliuk <kpavliuk8@gmail.com>
2023-09-17 23:23:16 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -63,29 +63,6 @@ export class App extends Component {
       <div>
         <h1>Phonebook</h1>
         <ContactForm createContact={this.createContact} />
-
-        {/* <form onSubmit={this.handleSubmit}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            //pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            onChange={this.handleChange}
-            required
-          />
-          <label htmlFor="tel">Number</label>
-          <input
-            type="tel"
-            name="number"
-            //pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            onChange={this.handleChange}
-            required
-          />
-          <button type="submit">Add contact</button>
-        </form> */}
-
         <h2>Contacts</h2>
         <Filter handleChange={this.handleChange} />
         <ContactList
@@ -93,15 +70,6 @@ export class App extends Component {
           contacts={this.state.contacts}
           onDeleteClick={this.onDeleteClick}
         />
-        {/* <p>Find contacts by name</p>
-        <input type="text" name="filter" onChange={this.handleChange} />
-        <ul>
-          {filteredContacts.map(contact => (
-            <li key={contact.id}>
-              {contact.name} {contact.number}
-            </li>
-          ))}
-        </ul> */}
       </div>
     );
   }
